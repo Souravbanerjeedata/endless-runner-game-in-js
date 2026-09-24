a4532f00ca6e876abd0522bd5d979a18b4b02f7e
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-09-24 13:29:30 +0530

    added all the states and moving frames

--- main.js
+++ main.js
@@ -14,20 +14,23 @@ window.addEventListener("load", () => {
       this.player = new Player(this);
       this.input = new InputHandler();
     }
-    update() {
-      this.player.update(this.input.keys);
+    update(deltaTime) {
+      this.player.update(this.input.keys, deltaTime);
     }
     draw(context) {
       this.player.draw(context);
     }
   }
   const game = new Game(canvas.width, canvas.height);
+  let lastTime = 0;
 
-  function animate() {
+  function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    game.update();
+    game.update(deltaTime);
     game.draw(ctx);
     requestAnimationFrame(animate);
   }
-  animate();
+  animate(0);
 });
--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-import { Sitting, Running } from "./playerStates.js";
+import { Sitting, Running, Jumping, Falling } from "./playerStates.js";
 
 export class Player {
   constructor(game) {
@@ -11,14 +11,23 @@ export class Player {
     this.image = document.getElementById("player");
     this.frameX = 0;
     this.frameY = 0;
+    this.maxFrame;
+    this.fps = 20;
+    this.frameInterval = 1000 / this.fps;
+    this.frameTimer = 0;
     this.speed = 0;
     this.maxSpeed = 10;
     this.weight = 1;
-    this.states = [new Sitting(this), new Running(this)];
+    this.states = [
+      new Sitting(this),
+      new Running(this),
+      new Jumping(this),
+      new Falling(this),
+    ];
     this.currentState = this.states[0];
     this.currentState.enter();
   }
-  update(input) {
+  update(input, deltaTime) {
     this.currentState.handleInput(input);
     // horizontal movement
     this.x += this.speed;
@@ -29,10 +38,18 @@ export class Player {
     else if (this.x > this.game.width - this.width)
       this.x = this.game.width - this.width;
     // vertical movement
-    if (input.includes("ArrowUp") && this.onGround()) this.vy -= 20;
+
     this.y += this.vy;
     if (!this.onGround()) this.vy += this.weight;
     else this.vy = 0;
+    // sprite animation
+    if (this.frameTimer > this.frameInterval) {
+      this.frameTimer = 0;
+      if (this.frameX < this.maxFrame) this.frameX++;
+      else this.frameX = 0;
+    } else {
+      this.frameTimer += deltaTime;
+    }
   }
   draw(context) {
     context.drawImage(
