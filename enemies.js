eed211c58389e245137105993ff43be7ca232304
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-09-25 18:59:15 +0530

    spawning 3 different enemies

--- enemies.js
+++ enemies.js
@@ -9,7 +9,7 @@ class Enemy {
   }
   update(deltaTime) {
     // movement
-    this.x -= this.speedX;
+    this.x -= this.speedX + this.game.speed;
     this.y += this.speedY;
     if (this.frameTimer > this.frameInterval) {
       this.frameTimer = 0;
@@ -42,18 +42,61 @@ export class FlyingEnemy extends Enemy {
     this.game = game;
     this.width = 60;
     this.height = 44;
-    this.x = this.game.width;
+    this.x = this.game.width + Math.random() * this.game.width * 0.5;
     this.y = Math.random() * this.game.height * 0.5;
     this.speedX = 2;
     this.speedY = 0;
     this.maxFrame = 5;
     this.image = document.getElementById("enemy_fly");
+    this.angle = 0;
+    this.vs = Math.random() * 0.1 + 0.1;
   }
   update(deltaTime) {
     super.update(deltaTime);
+    this.angle += this.vs;
+    this.y += Math.sin(this.angle);
   }
 }
 
-export class GroundEnemy extends Enemy {}
+export class GroundEnemy extends Enemy {
+  constructor(game) {
+    super();
+    this.game = game;
+    this.width = 60;
+    this.height = 87;
+    this.x = this.game.width;
+    this.y = this.game.height - this.height - this.game.groundMargin;
+    this.image = document.getElementById("enemy_plant");
+    this.speedX = 0;
+    this.speedY = 0;
+    this.maxFrame = 1;
+  }
+}
 
-export class ClimbingEnemy extends Enemy {}
+export class ClimbingEnemy extends Enemy {
+  constructor(game) {
+    super();
+    this.game = game;
+    this.width = 120;
+    this.height = 144;
+    this.x = this.game.width;
+    this.y = Math.random() * this.game.height * 0.5;
+    this.image = document.getElementById("enemy_spider_big");
+    this.speedX = 0;
+    this.speedY = Math.random() > 0.5 ? 1 : -1;
+    this.maxFrame = 5;
+  }
+  update(deltaTime) {
+    super.update(deltaTime);
+    if (this.y > this.game.height - this.height - this.game.groundMargin)
+      this.speedY *= -1;
+    if (this.y < -this.height) this.markedForDeletion = true;
+  }
+  draw(context) {
+    super.draw(context);
+    context.beginPath();
+    context.moveTo(this.x + this.width / 2, 0);
+    context.lineTo(this.x + this.width / 2, this.y + 50);
+    context.stroke();
+  }
+}
