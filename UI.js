2d25e3582e1d2a856333d3aff03c6796e0fc51c4
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-09-26 17:07:08 +0530

    game over text

--- UI.js
+++ UI.js
@@ -5,10 +5,49 @@ export class UI {
     this.fontFamily = "Helvetica";
   }
   draw(context) {
+    context.save();
+    context.shadowOffsetX = 2;
+    context.shadowOffsetY = 2;
+    context.shadowColor = "white";
+    context.shadowBlur = 0;
     context.font = this.fontSize + "px " + this.fontFamily;
     context.textAlign = "left";
     context.fillStyle = this.game.fontColor;
     // score
     context.fillText("Score: " + this.game.score, 20, 50);
+    // timer
+    context.font = this.fontSize * 0.8 + "px " + this.fontFamily;
+    context.fillText("Time: " + (this.game.time * 0.001).toFixed(1), 20, 80);
+    // gameover message
+    if (this.game.gameOver) {
+      context.textAlign = "center";
+      context.font = this.fontSize * 2 + "px " + this.fontFamily;
+      if (this.game.score > 5) {
+        context.fillText(
+          "Victory!",
+          this.game.width * 0.5,
+          this.game.height * 0.5 - 20,
+        );
+        context.font = this.fontSize * 0.7 + "px " + this.fontFamily;
+        context.fillText(
+          "They did not know who they were dealing with!",
+          this.game.width * 0.5,
+          this.game.height * 0.5 + 20,
+        );
+      } else {
+        context.fillText(
+          "You Failed.",
+          this.game.width * 0.5,
+          this.game.height * 0.5 - 20,
+        );
+        context.font = this.fontSize * 0.7 + "px " + this.fontFamily;
+        context.fillText(
+          "Better luck next time!",
+          this.game.width * 0.5,
+          this.game.height * 0.5 + 20,
+        );
+      }
+    }
+    context.restore();
   }
 }
